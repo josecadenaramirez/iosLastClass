fc95dab05073cb9fd11e557a1c948f52ee246fe4
José Cadena <cadena@tejuino.mx>
2018-07-16 20:52:29 -0500

    Initial Commit

---
+++ finalClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalClass
+//
+//  Created by José Cadena on 16/07/18.
+//  Copyright © 2018 José Cadena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
